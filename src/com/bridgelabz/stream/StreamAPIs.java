acf23e04f6fba33bfab002538aaee5b505e052b0
Roshni1998 <roshnimali847@gmail.com>
2021-06-23 21:48:54 +0530

    Added: Stream API UC-2.2 & UC-2.3

--- src/com/bridgelabz/stream/StreamAPIs.java
+++ src/com/bridgelabz/stream/StreamAPIs.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class StreamAPIs {
     public static void main (String []args){
@@ -65,6 +66,11 @@ public class StreamAPIs {
         myNumberList.stream().forEach(n->{
             System.out.println("Method 8: Stream forEach Value :: "+n);
         });
+
+        //Method 9: Process the Stream, Apply Operations on the Stream and then
+        // Store the Result
+        List<Double> streamList = myNumberList.stream().map(toDoubleFunction).collect(Collectors.toList());
+        System.out.println("Method 9: Printing Double List: "+streamList);
     }
 
 }
